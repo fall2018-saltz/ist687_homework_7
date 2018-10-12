5a47ae99422955994f69eeff67d67f802506fcb0
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 23:54:05 -0400

    Saving map1.R

--- map1.R
+++ map1.R
@@ -1,12 +1,13 @@
 
 # Create a color coded map, based on the area of the state 
 
-# this uses the libraries ggplot2 and ggmap
+# this uses the libraries the following libraries
 library("ggplot2")
 library("ggmap")
 library("maps")
 library("mapdata")
-#
+
+#map_data turns a s a function that turns a series of points along an outline into a data frame of those points.
 us <- map_data("state")
 options(scipen=999)
 map1 <- ggplot(clean_data_merged, aes(map_id = stateName))
