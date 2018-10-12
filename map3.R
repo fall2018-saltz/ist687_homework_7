dd00c9d0a66977f5cc58ad061f4cbc0d8ae04c4e
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-12 00:51:46 -0400

    Saving map3.R

--- map3.R
+++ map3.R
@@ -11,6 +11,8 @@ options(scipen=999)
 map3 <- ggplot(clean_data_merged, aes(map_id = stateName))
 map3 <- map3 + geom_map(map = us,aes(fill=Murder))+ scale_fill_gradient(low = "white", high = "red")             
 map3 <- map3 + expand_limits(x = us$long, y = us$lat) + coord_map()
+
+# use geom_point to plot the points at the center of the states and adjust the size of the 
 map3 <- map3 + geom_point(data = clean_data_merged, mapping = aes(x = statecenterx, y = statecentery, size=population))
 map3
 
