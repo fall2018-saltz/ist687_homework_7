acb08bb716bc7c1532e72d38762d278ce755fdc2
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 21:32:52 -0400

    Saving map1.R

--- map1.R
+++ map1.R
@@ -3,5 +3,5 @@ library("ggplot2")
 library("ggmap")
 us <- map_data("state")
 map1 <- ggplot(clean_data_merged, aes(map_id = clean_data_merged$stateName))
-map1 <- map1 + geom_map(map = us ,fill="white", color=statearea)
+map1 <- map1 + geom_map(map = us ,fill="white", color=state.area)
 map1
