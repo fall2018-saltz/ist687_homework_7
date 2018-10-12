9a9f89f976c29e8a5acc4d63cf80887346ec1559
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 20:37:57 -0400

    Saving merge_data.R

--- merge_data.R
+++ merge_data.R
@@ -16,9 +16,9 @@ cleandata2$stateName <- x
 clean_data_merged <- merge(clean_data,cleandata2,by="stateName")
 
 
-clean_data_merged$statearea <- as.vector(state.area)
-clean_data_merged$statecenterx <- as.vector(state.center$x)
-clean_data_merged$statecentery <- as.vector(state.center$y)
-clean_data_merged$statename <- as.vector(state.name)
+clean_data_merged <- c(clean_data_merged,state.area)
+#clean_data_merged$statecenterx <- as.vector(state.center$x)
+#clean_data_merged$statecentery <- as.vector(state.center$y)
+#clean_data_merged$statename <- as.vector(state.name)
 
 tail(clean_data_merged)
