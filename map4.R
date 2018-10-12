a55046e54192dae45f7f75cf66e644acda9e1573
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 23:09:04 -0400

    Saving map4.R

--- map4.R
+++ map4.R
@@ -13,4 +13,4 @@ map4 <- map4 + geom_map(map = us,aes(fill=Murder))+ scale_fill_gradient(low = "w
 map4 <- map4 + expand_limits(x = us$long, y = us$lat) + coord_map()
 map4 <- map4 + geom_point(data = clean_data_merged, mapping = aes(x = statecenterx, y = statecentery, size=population))
 map4 <- map4 + xlim(x1,x2) + ylim(y1,y2)
-
+map4
