1858bc1bba9067c62df4ae0446e0d676c565dc75
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 21:38:46 -0400

    Saving map1.R

--- map1.R
+++ map1.R
@@ -3,6 +3,6 @@ library("ggplot2")
 library("ggmap")
 us <- map_data("state")
 map1 <- ggplot(clean_data_merged, aes(map_id = stateName))
-map1 <- map1 + geom_map(map = us ,aex(fill= state.area))
+map1 <- map1 + geom_map(map = us ,aes(fill= state.area))
 map1 <- map1 + expand_limits(x = us$long, y = us$lat)
 map1
