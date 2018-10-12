8eb737ff85ed1a7c5ab5834ce9304bbda29ac3d5
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 20:46:52 -0400

    Saving merge_data.R

--- merge_data.R
+++ merge_data.R
@@ -15,8 +15,8 @@ cleandata2$stateName <- x
 # here it merges the dataframes by the common column stateName
 clean_data_merged <- merge(clean_data,cleandata2,by="stateName")
 
-clean_data_merged$statearea <- NA
-clean_data_merged$statearea <- as.vector(state.area)
+
+clean_data_merged <- cbind(clean_data_merged,state.area)
 #clean_data_merged$statecenterx <- as.vector(state.center$x)
 #clean_data_merged$statecentery <- as.vector(state.center$y)
 #clean_data_merged$statename <- as.vector(state.name)
