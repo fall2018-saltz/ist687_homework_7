0d9e72c4f148c103e240b284ee1451e09fccd9d2
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 23:04:02 -0400

    Saving map4.R

--- map4.R
+++ map4.R
@@ -5,9 +5,10 @@ us <- map_data("state")
 options(scipen=999)
 latlon <- geocode("new york city, ny",source="dsk")
 x1 <- latlon$lat-10
+y1 <- latlon$lon-10
 y2 <- latlon$lon+10
 x2 <- latlon$lat+10
-map4 <- ggplot(clean_data_merged, aes(map_id = stateName,xlim=x,ylim=y))
+map4 <- ggplot(clean_data_merged, aes(map_id = stateName,xlim=c(x1,x2),ylim=(y1,y2))
 map4 <- map4 + geom_map(map = us,aes(fill=Murder))+ scale_fill_gradient(low = "white", high = "red")             
 map4 <- map4 + expand_limits(x = us$long, y = us$lat) + coord_map()
 map4 <- map4 + geom_point(data = clean_data_merged, mapping = aes(x = statecenterx, y = statecentery, size=population))
