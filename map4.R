e02465ea49fb19b56974050ff4960077c15fe7ce
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-12 00:54:45 -0400

    Saving map4.R

--- map4.R
+++ map4.R
@@ -8,7 +8,7 @@ library("ggmap")
 us <- map_data("state")
 options(scipen=999)
 
-# get the latitude and longitude from the datasciencetoolkit 
+# get the latitude and longitude of NYC from the datasciencetoolkit 
 latlon <- geocode("new york city, ny",source="dsk")
 y1 <- latlon$lat-10
 x1 <- latlon$lon-10
