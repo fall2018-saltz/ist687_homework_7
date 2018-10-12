0f53bea7e0485a3e96a3e94a54d086e32182f512
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 23:10:07 -0400

    Saving map4.R

--- map4.R
+++ map4.R
@@ -12,5 +12,5 @@ map4 <- ggplot(clean_data_merged, aes(map_id = stateName))
 map4 <- map4 + geom_map(map = us,aes(fill=Murder))+ scale_fill_gradient(low = "white", high = "red")             
 map4 <- map4 + expand_limits(x = us$long, y = us$lat) + coord_map()
 map4 <- map4 + geom_point(data = clean_data_merged, mapping = aes(x = statecenterx, y = statecentery, size=population))
-map4 <- map4 + xlim(x1,x2) + ylim(y1,y2)
+map4 <- map4 + xlim(x2,x1) + ylim(y2,y1)
 map4
