eea376f59a0abbcd625dd43dbe04f0e513bbc21d
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 22:17:13 -0400

    Saving map3.R

--- map3.R
+++ map3.R
@@ -1,2 +1,11 @@
 
-geom_point(data = bc, mapping = aes(x = longitude, y = latitude, color = region))
+library("ggplot2")
+library("ggmap")
+us <- map_data("state")
+options(scipen=999)
+map2 <- ggplot(clean_data_merged, aes(map_id = stateName))
+map2 <- map2 + geom_map(map = us,aes(fill=Murder))+ scale_fill_gradient(low = "black", high = "red")             
+map2 <- map2 + expand_limits(x = us$long, y = us$lat) + coord_map()
+map2 <- map2 + geom_point(data = clean_data_merged, mapping = aes(x = statecenterx, y = statecentery, fill = "green",size=Population))
+
+
