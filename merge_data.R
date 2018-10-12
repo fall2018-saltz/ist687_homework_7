8d942738ee97805ea19624746dd7bb0ee5c94827
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 20:59:02 -0400

    Saving merge_data.R

--- merge_data.R
+++ merge_data.R
@@ -5,16 +5,15 @@ cleandata2 <- USArrests
 
 
 # save row names as a separate variable
-x <- as.vector(rownames(USArrests))
+stateName <- as.vector(rownames(USArrests))
 
 # add this column to the dataframe as a column names stateName
-cleandata2 <- cbind(cleandata2,x, stringsAsFactors=FALSE)
-#cleandata2$x<- droplevels(cleandata2$x)
-str(cleandata2)
+cleandata2 <- cbind(cleandata2,stateName, stringsAsFactors=FALSE)
+
 # merge the dataframes based on the stateName column in both the dataframes
 # the merge function combines dataframes based on common columns
 # here it merges the dataframes by the common column stateName
-#clean_data_merged <- merge(clean_data,cleandata2,by="stateName")
+clean_data_merged <- merge(clean_data,cleandata2,by="stateName")
 
 
 #clean_data_merged <- cbind(clean_data_merged,as.data.frame(state.area))
