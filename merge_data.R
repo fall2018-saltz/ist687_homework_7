a76f84925260951d5adef040a1e77a1d7c394a54
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 20:56:20 -0400

    Saving merge_data.R

--- merge_data.R
+++ merge_data.R
@@ -10,6 +10,7 @@ x <- as.vector(rownames(USArrests))
 # add this column to the dataframe as a column names stateName
 cleandata2 <- cbind(cleandata2,x)
 cleandata2<- droplevels(cleandata2)
+str(cleandata2)
 # merge the dataframes based on the stateName column in both the dataframes
 # the merge function combines dataframes based on common columns
 # here it merges the dataframes by the common column stateName
