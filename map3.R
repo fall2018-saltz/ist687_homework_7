197fb350416f3da050e1a07f98a51aea5f9b7493
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-12 01:05:17 -0400

    Saving map3.R

--- map3.R
+++ map3.R
@@ -15,5 +15,5 @@ map3 <- map3 + expand_limits(x = us$long, y = us$lat) + coord_map()
 # use geom_point to plot the points at the center of the states and 
 # adjust the size of the points according to the population
 map3 <- map3 + geom_point(data = clean_data_merged, mapping = aes(x = statecenterx, y = statecentery, size=population))
-map3
+map3 <- map3 + ggtitle("population as points")
 
